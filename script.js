fef12e06fcec760469454fcdc3e5e520d32ba517
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-05-16 16:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digitstr) {
   //Complete the function
+	 digitstr = "23";
+let output = [];
+function solve(index,str){
+if(index >= digitstr.length){
+  output.push(str);
+  return;
+}
+let currentstr = obj[digitstr[index]];
+for(let i = 0; i < currentstr.length; i++){
+  solve(index + 1,str + currentstr[i]);
+}
 }
 
+solve(0,"");
+console.log(output);
+}
+letterCombinations("23")
 module.exports = letterCombinations;
